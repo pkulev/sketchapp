79939cd3fd21125f05fe5060f1014339f8ff337b
Pavel Kulyov <kulyov.pavel@gmail.com>
2020-10-29 19:33:18 +0300

    infra: bump monger version
    Signed-off-by: Pavel Kulyov <kulyov.pavel@gmail.com>

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "MIT"
             :url "https://mit-license.org/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.novemberain/monger "3.1.0"]]
+                 [com.novemberain/monger "3.5.0"]]
   :plugins [[lein-cljfmt "0.7.0"]
             [lein-codox "0.10.7"]]
   :repl-options {:init-ns sketchapp.core}
