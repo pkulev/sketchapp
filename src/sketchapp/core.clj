55d0acbff25988fa04eba6dc0006dfaca5305aff
Pavel Kulyov <kulyov.pavel@gmail.com>
2020-10-29 18:22:49 +0300

    Add example of monger usage
    Signed-off-by: Pavel Kulyov <kulyov.pavel@gmail.com>

--- src/sketchapp/core.clj
+++ src/sketchapp/core.clj
@@ -1,6 +1,23 @@
-(ns sketchapp.core)
+(ns sketchapp.core
+  (:require [monger.core :as mg]
+            [monger.collection :as mc]
+            [monger.query :refer :all])
+  (:use clojure.pprint))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn check-db
+  "Write something."
+  []
+  (let [conn (mg/connect {:host "db"})
+        db (mg/get-db conn "test-db")]
+    (mc/insert db "keks" {:kek_1 10 :kek_2 20})
+    (pprint (mc/find-one-as-map db "keks" {:kek_1 10}))))
+
+(defn main
+  "Entry point."
+  []
+  (check-db)
+  (pprint
+   (with-collection (mg/get-db (mg/connect {:host "db"}) "test-db") "keks"
+     (find {})
+     (fields [:kek_1])
+     (limit 10))))
