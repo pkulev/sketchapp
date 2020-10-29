b4b35e431f4b0602e76097d533d48b9d70fcc439
Pavel Kulyov <kulyov.pavel@gmail.com>
2020-10-29 18:21:24 +0300

    Add monger and provide main function
    Signed-off-by: Pavel Kulyov <kulyov.pavel@gmail.com>

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "https://github.com/pkulev/sketchapp"
   :license {:name "MIT"
             :url "https://mit-license.org/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.novemberain/monger "3.1.0"]]
   :plugins [[lein-cljfmt "0.7.0"]
             [lein-codox "0.10.7"]]
-  :repl-options {:init-ns sketchapp.core})
+  :repl-options {:init-ns sketchapp.core}
+  :main sketchapp.core/main
+  :source-paths ["src"])
